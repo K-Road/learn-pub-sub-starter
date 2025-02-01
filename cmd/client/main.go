5fc28228fec7b2f10f1979282105c753a0785919
K-Road <game@chrodeprime.com>
2025-02-02 02:42:38 +1100

    added queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const rabbit_connection_string = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbit_connection_string)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful")
+
+	user, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("failed to get user: %v", err)
+	}
+
+	ch, q, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, user), routing.PauseKey, 0)
+	if err != nil {
+		log.Fatalf("failing to subscribe to pause: %v", err)
+	}
+	defer ch.Close()
+	fmt.Printf("Queue %v declared and bound!\n", q.Name)
+
+	fmt.Println(q)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Printf("Shutting down\n")
+
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const rabbit_connection_string = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbit_connection_string)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not open channel: %v", err)
+	}
+	defer ch.Close()
+
+	playingState := routing.PlayingState{
+		IsPaused: true,
+	}
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, playingState)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Printf("Shutting down\n")
+
 }
